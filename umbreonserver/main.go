4658f56b1a9255cc135d33b6f66b5e14bcf71ec3
Jordan Mackie <jmackie97@hotmail.com>
2021-08-01 12:57:06 +0100

    serve assets

--- umbreonserver/main.go
+++ umbreonserver/main.go
@@ -13,7 +13,7 @@ func main() {
 	router := gin.Default()
 
 	// Serve frontend static files
-	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+	router.Use(static.Serve("/", static.LocalFile("./assets", true)))
 
 	// Setup route group for the API
 	api := router.Group("/api")

3cad9db322d1f24952df54d45c59263f8d0c7014
Jordan Mackie <jmackie97@hotmail.com>
2021-08-01 01:00:05 +0100

    push latest tag

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -13,12 +13,16 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+//
+//resource "aws_instance" "api-server" {
+//  ami           = data.aws_ami.ubuntu.id
+//  instance_type = "t3.micro"
+//
+//  tags = {
+//    Name = "UmbreonServer"
+//  }
+//}
 
-resource "aws_instance" "api-server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "UmbreonServer"
-  }
+resource "aws_ecs_cluster" "umbreon_cluster" {
+  name = "umbreon-cluster"
 }

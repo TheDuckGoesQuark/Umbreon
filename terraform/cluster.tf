5f5f9fd23c13e07486c4703dddacbe6542cacd7c
Jordan Mackie <jmackie97@hotmail.com>
2021-08-01 02:20:27 +0100

    reduce to 1, need to figure out why builds don't pass the deploy stage

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -93,7 +93,7 @@ resource "aws_ecs_service" "umbreon_service" {
   cluster         = "${aws_ecs_cluster.umbreon_cluster.id}"             # Referencing our created Cluster
   task_definition = "${aws_ecs_task_definition.umbreon_ecs_task.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers we want deployed to 3
+  desired_count   = 1 # Setting the number of containers we want deployed to 3
 
   load_balancer {
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
